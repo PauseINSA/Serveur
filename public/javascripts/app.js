0e2e8f308d62d2c9859bd0c3c9ad131d7e32c49c
tlk <jeremieboutoille@gmail.com>
2014-01-25 15:31:31 +0100

    Adding some handles

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,23 @@
 
 var machines = {};
+var userPosition;
 var socket = io.connect();
+
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(successGeoloc, errorGeoloc);
+// } else {
+//     error('not supported');
+// }
+// 
+// function successGeoloc(position)
+// {
+//     userPosition = position;
+// }
+// 
+// function errorGeoloc(msg)
+// {
+//     console.log(msg);
+// }
 
 /*
  * Lorsque l'on reçoit les données initiales à la connexion
@@ -38,11 +55,71 @@ $("#btn-boisson").click(function(){
 });
 
 /*
- * Demande d'affichage de toutes les machines
+ * Lorsque l'utilisateur clique sur le nom d'une machine
+ */
+$("#liste-machine").delegate('li', 'click', function(){
+    var id = $(this).attr('id');
+    var machine = getMachineById(id);
+    console.log(machine);
+    $("#popup-infos div.ui-content").empty();
+    $('<p>'+machine.description+'</p>').appendTo("#popup-infos div.ui-content");
+});
+
+/*
+ * Demande d'affichage par boisson
+ */
+$("#menu-display-boisson").click(function(){
+    $("#nav-panel").panel('close');
+});
+
+/*
+ * Demande d'affichage de toutes les machines par batiment
  */
 $("#menu-display-batiment").click(function(){
     displayMachinesByBatiment();
 });
+
+/*
+ * Demande d'affichage de toutes les machines par type
+ */
+$("#menu-display-type").click(function(){
+    displayMachinesByType();
+});
+
+/*
+ * Afficher toutes les machines en triant par type de machine
+ */
+function displayMachinesByType()
+{
+    $("#liste-machine").empty();
+    $.each(machines, function(){
+        addMachineByType(this);
+    });
+    $("#liste-machine").listview('refresh');
+}
+
+/*
+ * Ajouter une machine à la liste en mettant son type comme séparateur
+ */
+function addMachineByType(machine)
+{
+    if($("li#type-"+machine['type']).length == 0)
+    {
+        var name = "Machine a café";
+        if(machine['type'] == 'canette')
+        {
+            name = "Machine a canette";
+        }
+        $('<li data-role="list-divider" id="type-'+machine['type']+'">'+name+'</li>').appendTo("#liste-machine");
+    }
+
+    $("<li id=\""+machine['id']+"\">"+
+        '<a href="#popup-infos" href="#popup-infos" data-rel="popup" data-position-to="window" data-transition="pop">' + 
+            '<img src="/images/'+getPointMachine(machine)+'-point.png" class="ui-li-icon"/>' + machine.batiment['nom'] + ' - ' + machine['description'] +
+        '</a>'+
+      '</li>').insertAfter('li#type-'+machine['type']);
+    
+}
 
 /*
  * Afficher toutes les machines en triant par bâtiment
@@ -64,15 +141,29 @@ function addMachineByBatiment(machine)
     // si le séparateur existe pas, on le créé
     if($("li#bat-"+machine.batiment['id']).length == 0)
     {
-        $("<li data-role=\"list-divider\" id=\"bat-"+machine.batiment['id']+"\">"+machine.batiment['nom']+"</ul>").appendTo("#liste-machine");
+        $("<li data-role=\"list-divider\" id=\"bat-"+machine.batiment['id']+"\">"+machine.batiment['nom']+"</li>").appendTo("#liste-machine");
     }
 
     // on ajoute la machine juste après le bon séparateur
-    $("<li>"+
-            "<a href=\"#page-infos\" data-transition=\"slide\">"+
-                "<img src=\"/images/"+getPointMachine(machine)+"-point.png\" class=\"ui-li-icon\"/>"+machine.description+
+    $("<li id=\""+machine['id']+"\">"+
+            '<a href="#popup-infos" data-rel="popup" data-position-to="window" data-transition="pop">'+
+                "<img src=\"/images/"+getPointMachine(machine)+"-point.png\" class=\"ui-li-icon\"/>"+machine['description']+
             "</a>" +
       "</li>").insertAfter("li#bat-"+machine.batiment['id'])
+}
+
+/*
+ * Renvoie toutes les infos d'une machine en fonction de son id
+ */
+function getMachineById(id)
+{
+    for(i in machines)
+    {
+        if(machines[i].id == id)
+        {
+            return machines[i];
+        }
+    }
 }
 
 /*
