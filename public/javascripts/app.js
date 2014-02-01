b8426169bc5f429ca85af4084fc68f7963741231
tlk <jeremieboutoille@gmail.com>
2014-02-01 10:32:48 +0100

    Update client app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,30 +1,15 @@
 
-var machines = {};
-var userPosition;
-var socket = io.connect();
 
-// if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(successGeoloc, errorGeoloc);
-// } else {
-//     error('not supported');
-// }
-// 
-// function successGeoloc(position)
-// {
-//     userPosition = position;
-// }
-// 
-// function errorGeoloc(msg)
-// {
-//     console.log(msg);
-// }
+var machines;
+var socket = io.connect();
 
 /*
  * Lorsque l'on reçoit les données initiales à la connexion
  */
 socket.on('updateAll', function(data){
-    machines = data;
-    displayMachinesByBatiment();
+    machines = new Machines();
+    machines.parseJson(data);
+    machines.displayByBatiment();
 });
 
 /*
@@ -32,156 +17,26 @@ socket.on('updateAll', function(data){
  */
 $("#btn-boisson").click(function(){
     var boisson = $("#select-boisson").val();
-    // on vide la liste courrante
-    $("#liste-machine").empty();
-    // on itère sur chaque machine
-    $.each(machines, function(){
-        // on a choisi un café, que la machine courrante est une cafetière et qu'elle marche, on ajoute dans la liste
-        if(boisson == 'cafe' && this.type == 'cafe' && this.status == true)
-        {
-            addMachineByBatiment(this);
-        // une autre boisson et la machine vend des canettes
-        } else if (boisson != 'cafe' && this.type == 'canette'){
-            for(i in this.boissons) // pour chaque boisson dans la machine
-            {
-                if(this.boissons[i].nom.toLowerCase() == boisson && this.boissons[i].status == true) // la boisson est dispo
-                {
-                    addMachineByBatiment(this); // on ajoute à la liste
-                }
-            }
-        }
-    });
-    $("#liste-machine").listview('refresh');
+    machines.displayByBoisson(boisson);
 });
 
 /*
  * Lorsque l'utilisateur clique sur le nom d'une machine
  */
 $("#liste-machine").delegate('li', 'click', function(){
-    var id = $(this).attr('id');
-    var machine = getMachineById(id);
-    console.log(machine);
-    $("#popup-infos div.ui-content").empty();
-    $('<p>'+machine.description+'</p>').appendTo("#popup-infos div.ui-content");
-});
 
-/*
- * Demande d'affichage par boisson
- */
-$("#menu-display-boisson").click(function(){
-    $("#nav-panel").panel('close');
 });
 
 /*
  * Demande d'affichage de toutes les machines par batiment
  */
 $("#menu-display-batiment").click(function(){
-    displayMachinesByBatiment();
+    machines.displayByBatiment();
 });
 
 /*
  * Demande d'affichage de toutes les machines par type
  */
 $("#menu-display-type").click(function(){
-    displayMachinesByType();
+    machines.displayByType();
 });
-
-/*
- * Afficher toutes les machines en triant par type de machine
- */
-function displayMachinesByType()
-{
-    $("#liste-machine").empty();
-    $.each(machines, function(){
-        addMachineByType(this);
-    });
-    $("#liste-machine").listview('refresh');
-}
-
-/*
- * Ajouter une machine à la liste en mettant son type comme séparateur
- */
-function addMachineByType(machine)
-{
-    if($("li#type-"+machine['type']).length == 0)
-    {
-        var name = "Machine a café";
-        if(machine['type'] == 'canette')
-        {
-            name = "Machine a canette";
-        }
-        $('<li data-role="list-divider" id="type-'+machine['type']+'">'+name+'</li>').appendTo("#liste-machine");
-    }
-
-    $("<li id=\""+machine['id']+"\">"+
-        '<a href="#popup-infos" href="#popup-infos" data-rel="popup" data-position-to="window" data-transition="pop">' + 
-            '<img src="/images/'+getPointMachine(machine)+'-point.png" class="ui-li-icon"/>' + machine.batiment['nom'] + ' - ' + machine['description'] +
-        '</a>'+
-      '</li>').insertAfter('li#type-'+machine['type']);
-    
-}
-
-/*
- * Afficher toutes les machines en triant par bâtiment
- */
-function displayMachinesByBatiment()
-{
-    $("#liste-machine").empty();
-    $.each(machines, function(){
-        addMachineByBatiment(this);
-    });
-    $("#liste-machine").listview('refresh');
-}
-
-/*
- * Ajouter une machine à la liste en mettant les batiments comme séparateur
- */
-function addMachineByBatiment(machine)
-{
-    // si le séparateur existe pas, on le créé
-    if($("li#bat-"+machine.batiment['id']).length == 0)
-    {
-        $("<li data-role=\"list-divider\" id=\"bat-"+machine.batiment['id']+"\">"+machine.batiment['nom']+"</li>").appendTo("#liste-machine");
-    }
-
-    // on ajoute la machine juste après le bon séparateur
-    $("<li id=\""+machine['id']+"\">"+
-            '<a href="#popup-infos" data-rel="popup" data-position-to="window" data-transition="pop">'+
-                "<img src=\"/images/"+getPointMachine(machine)+"-point.png\" class=\"ui-li-icon\"/>"+machine['description']+
-            "</a>" +
-      "</li>").insertAfter("li#bat-"+machine.batiment['id'])
-}
-
-/*
- * Renvoie toutes les infos d'une machine en fonction de son id
- */
-function getMachineById(id)
-{
-    for(i in machines)
-    {
-        if(machines[i].id == id)
-        {
-            return machines[i];
-        }
-    }
-}
-
-/*
- * Renvoie la couleur du point à afficher devant la machine
- */
-function getPointMachine(machine)
-{
-    if(machine.status != true)
-    {
-        return "red";
-    } else if(machine.type == 'canette') {
-        for(i in machine.boissons)
-        {
-            if(machine.boissons[i].status == false)
-            {
-                return "orange";
-            }
-        }
-    }
-    return "green";
-}
---
+++ public/javascripts/objets.js
@@ -0,0 +1,264 @@
+/*
+ * Classe pour la gestion des machines
+ * Toutes les fonctions de bases sont gérées ici
+ */
+ function Machines()
+ {
+
+    this.machines = [];
+    this.listview = $("#liste-machine");
+
+    /*
+     * Parse le json renvoyé par le serv
+     */
+    this.parseJson = function(machines)
+    {
+        for(i in machines)
+        {
+            this.machines.push(new Machine(machines[i]));
+        }
+    }
+
+    /*
+     * Renvois un objet Machine en fonction de l'id
+     */
+    this.getMachineById = function(id)
+    {
+        for(i in this.machines)
+        {
+            if(this.machines[i].id == id)
+            {
+                return this.machines[i];
+            }
+        }
+    }
+
+    /*
+     * Affiche les machines dans la listview
+     * Si batname == true, alors le nom du batiment est aussi affiché
+     */
+    this.loadHtml = function(batname,data)
+    {
+        batname = (typeof batname == 'undefined' ? false : batname);
+        data = (typeof data == 'undefined' ? this.machines : data);
+        this.listview.empty();
+        for(i in data)
+        {
+            var html = data[i].getHtmlLi(batname);
+            this.listview.append(html);
+        }
+        this.listview.listview('refresh');
+    }
+
+    /*
+     * Affiche les machines en utilisant les batiments comme séparateur
+     * <=> Change le autoDividersSelector en this.dividerBatiment.
+     */
+    this.displayByBatiment = function()
+    {
+        this.machines.sort(this.sortBatiment);
+        this.loadHtml();
+        this.listview.listview({autodividersSelector: this.dividerBatiment});
+        this.listview.listview('refresh');
+    }
+
+    /*
+     * Affiche les machines en utilisant le type comme séparateur
+     */
+    this.displayByType = function()
+    {
+        this.machines.sort(this.sortType);
+        this.loadHtml(batname=true); // affiche aussi le nom du batiment
+        this.listview.listview({autodividersSelector: this.dividerType});
+        this.listview.listview('refresh');
+    }
+
+    /*
+     * Affiches les machines qui ont telle boisson
+     */
+    this.displayByBoisson = function(boisson)
+    {
+        var machs = [];
+        for(i in this.machines)
+        {
+            if(this.machines[i].hasBoisson(boisson))
+            {
+                machs.push(this.machines[i]);
+            }
+        }
+        this.loadHtml(batname=false, data=machs);
+        this.listview.listview({autodividersSelector: this.dividerBatiment});
+        this.listview.listview('refresh');
+    }
+
+    /*
+     * Divider pour afficher par batiment
+     * /!\ this = element du DOM courrant...
+     */
+    this.dividerBatiment = function(elt)
+    {
+        var machine = machines.getMachineById(elt.attr('id'));
+        return machine.batiment.nom;
+    }
+
+    /*
+     * Triage des éléments par batiment
+     */
+    this.sortBatiment = function(a, b)
+    {
+        var batA = machines.getMachineById($(a).attr('id')).batiment.nom;
+        var batB = machines.getMachineById($(b).attr('id')).batiment.nom;
+
+        if(batA > batB)
+        {
+            return -1;
+        }
+
+        if(batA < batB)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    /*
+     * Divider pour afficher par type de machine
+     */
+    this.dividerType = function(elt)
+    {
+        var machine = machines.getMachineById(elt.attr('id'));
+        if(machine.type == 'cafe')
+        {
+            return "Cafe";
+        } else {
+            return "Canette";
+        }
+        return "ERROR";
+    }
+
+    /*
+     * Triage des éléments par type de machine
+     */
+    this.sortType = function(a, b)
+    {
+        var typeA = machines.getMachineById($(a).attr('id')).type;
+        var typeB = machines.getMachineById($(b).attr('id')).type;
+
+        if(typeA > typeB)
+        {
+            return -1;
+        }
+
+        if(typeA < typeB)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+
+}
+
+/*
+ * Objet de type Machine
+ */
+function Machine(json)
+{
+    this.id = json['id'];
+    this.status = json['status'];
+    this.type = json['type'];
+    this.description = json['description'];
+    this.coords = json['coords'];
+    this.qrcode = json['qrcode'];
+    this.createdAt = json['createdAt'];
+    this.updatedAt = json['updatedAt'];
+    this.boissons = [];
+    for(i in json['boissons'])
+    {
+        this.boissons.push(new Boisson(json['boissons'][i]));
+    }
+    this.batiment = new Batiment(json['batiment']);
+
+    /*
+     * Retourne le html de la machine pour l'afficher dans la liste
+     */
+    this.getHtmlLi = function(batname)
+    {
+        var html = '<li id="'+this.id+'">';
+        html += '<a href="#popup-infos" data-rel="popup" data-position-to="window" data-transition="pop">';
+        html += '<img src="/images/'+this.getColorPoint()+'-point.png" class="ui-li-icon"/>';
+        if(batname == true)
+        {
+            html += this.batiment.nom + ' - ';
+        }
+        html += this.description;
+        html += '</a>';
+        html += '</li>';
+        return html;
+    }
+
+    /*
+     * Retourne la couleur du point a afficher en fonction du status de la machine
+     */
+     this.getColorPoint = function()
+     {
+        if(this.status != true)
+        {
+            return 'red';
+        } else if(this.type == 'canette')
+        {
+            for(i in this.boissons)
+            {
+                if(this.boissons[i].status == false)
+                {
+                    return 'orange';
+                }
+            }
+        }
+        return 'green';
+     }
+
+    /*
+     * Retourne vrai ou faux en fonction de si la machine a ou non la boisson
+     */
+    this.hasBoisson = function(boisson)
+    {
+        if(this.type == boisson && this.status == true) // cafe
+        {
+            return true;
+        } else {
+            for(i in this.boissons)
+            {
+                if(this.boissons[i].nom.toLowerCase() == boisson && this.boissons[i].status == true)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+/*
+ * Objet de type Batiment
+ */
+function Batiment(json)
+{
+    this.id = json['id'];
+    this.nom = json['nom'];
+    this.createdAt = json['createdAt'];
+    this.updatedAt = json['updatedAt'];
+}
+
+/*
+ * Objet de type Boisson
+ */
+function Boisson(json)
+{
+    this.id = json['id'];
+    this.nom = json['nom'];
+    this.status = json['status'];
+    this.createdAt = json['createdAt'];
+    this.updatedAt = json['updatedAt'];
+}
